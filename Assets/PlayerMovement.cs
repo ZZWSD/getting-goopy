b33844b8a1b661f0c62639c3ca174739995130cc
ZZWSD <125881299+ZZWSD@users.noreply.github.com>
2025-04-16 05:10:52 +0800

    重新修整人物移動
    1.新增物件
    2.場景布置

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,28 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    public float jumpForce = 8f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 取得鍵盤輸入
-        movement.x = Input.GetAxisRaw("Horizontal"); // A(-1) / D(1)
-        movement.y = Input.GetAxisRaw("Vertical");   // S(-1) / W(1)
-    }
-    void FixedUpdate()
-    {
-        // 物理移動，防止卡頓
-        rb.MovePosition(rb.position + movement * 5 * Time.fixedDeltaTime);
+        // 移動
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // 地面檢查
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // 跳躍
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
+
